be5ea65760d453c2b1d94cbebedecfa0bb33d942
raghav <raghavtripurwar2016@gmail.com>
2019-09-08 14:25:16 +0530

    init commit

---
+++ playground/mongo-find.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
+	if(err){
+		return console.log('Unable to connect to mongodb server');
+	}
+	console.log('connect to mongodb server');
+
+	// db.collection('Todos').find({_id:new ObjectID('5d749dd0f6ceb22d505ba93b')}).toArray().then((docs)=>{
+	// 	console.log('Todos');
+	// 	console.log(JSON.stringify(docs,undefined,2));
+	// },(err)=>{
+	// 	console.log('Unable to fetch Todos', err);
+	// });
+	// db.collection('Todos').find().count().then((count)=>{
+	// 	console.log(`Todos count:${count}`);
+	// 	console.log(JSON.stringify(count,undefined,2));
+	// },(err)=>{
+	// 	console.log('Unable to fetch Todos', err);
+	// });
+
+	db.collection('Users').find({name:'raghav'}).toArray().then((docs)=>{
+		console.log(JSON.stringify(docs,undefined,2));
+	});
+
+	// db.close();
+});
